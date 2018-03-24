6d61fb39b43e7be50f34fd17f4713cf2699895d9
jazz8603 <jazz8603@gmail.com>
2018-03-23 18:33:25 -0700

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            foreach (var item in ReverseInt(4434221))
+            {
+                Console.Write(item);
+            }
+        }
+
+
+
+        public static List<int> ReverseInt(int a)
+        {
+            List<int> reversed = new List<int>();
+
+            int leftInt = a / 10;
+            int rightInt = a % 10;
+
+            reversed.Add(rightInt);
+
+            while (leftInt > 9)
+            {
+
+                rightInt = leftInt % 10;
+                leftInt = leftInt / 10;
+                reversed.Add(rightInt);
+
+            }
+            reversed.Add(leftInt);
+            return reversed;
+        }
+
+    }
+}
